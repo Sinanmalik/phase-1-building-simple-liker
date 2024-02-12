d2af8ee402ca0d300fc635e13489d1cbbad1565e
Sinanmalik <sinanmalik3113@gmail.com>
2024-02-12 18:34:56 -0500

    complete lab

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+// Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
 
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+document.addEventListener('DOMContentLoaded', () => {
+  // When a user clicks on an empty heart
+  document.querySelectorAll('.like-glyph').forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          // When the "server" returns a success status
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+        .catch(error => {
+          // When the "server" returns a failure status
+          errorModal.classList.remove('hidden');
+          const errorMessage = document.getElementById('modal-message');
+          errorMessage.innerText = error;
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
   });
-}
+
+  // When a user clicks on a full heart
+  document.querySelectorAll('.activated-heart').forEach(fullHeart => {
+    fullHeart.addEventListener('click', () => {
+      fullHeart.innerText = EMPTY_HEART;
+      fullHeart.classList.remove('activated-heart');
+    });
+  });
+});
